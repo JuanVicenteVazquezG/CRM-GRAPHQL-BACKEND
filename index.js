b47d11c37d751cb9a49827e30436116a92399ba1
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-07-05 14:29:32 +0200

    Moved Resolvers and typeDefs to files

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-const { ApolloServer, gql } = require('apollo-server');
-
-// Seed Proof
-const Courses = [
-    {
-        title: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        technology: 'JavaScript ES6',
-    },
-    {
-        title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        technology: 'React',
-    },
-    {
-        title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-        technology: 'Node.js'
-    },
-    {
-        title: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-        technology: 'React'
-    }
-];
-
-
-
-
-// Resolvers
-const resolvers = {
-    Query: {
-        obtainCourses: () => Courses,
-        obtainTechnologies: () => Courses,
-    }
-}
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./db/schema.graphql');
+const resolvers = require('./db/resolvers');
 
 //Server
 const server = new ApolloServer({
